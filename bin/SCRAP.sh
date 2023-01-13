13bdd98c837352abe9526c090e59deddc56d3b0f
sreenieadara <sreeni.eadara@gmail.com>
2023-01-13 14:16:21 -0500

    File check development in progress

--- bin/SCRAP.sh
+++ bin/SCRAP.sh
@@ -126,16 +126,17 @@ if [ $paired_end == "yes" ];
 then 
 
    if [ ! -f ${directory}${sample}/${sample}_R1.fastq.gz] || [ ! -f ${directory}${sample}/${sample}_R2.fastq.gz ]; then
-   echo "Paired-end reads (SAMPLE_R1.fastq.gz or SAMPLE_R2.fastq.gz) do not exist for ${sample}! Skipping . . ."
+      echo "Paired-end reads (SAMPLE_R1.fastq.gz or SAMPLE_R2.fastq.gz) do not exist for ${sample}! Skipping QC . . ."
 
-   continue
-
+      continue
+   fi
 else
 
    if [ ! -f ${directory}${sample}/${sample}.fastq.gz]; then
-   echo "Reads (SAMPLE.fastq.gz) do not exist for ${sample}! Skipping . . ."
+      echo "Reads (SAMPLE.fastq.gz) do not exist for ${sample}! Skipping QC. . ."
 
-   continue
+      continue
+   fi
 fi
 
 #Run FastQC
@@ -167,6 +168,23 @@ done
 
 for sample in $samples
 do
+
+#Check if sequence files exist - otherwise skip the sample
+if [ $paired_end == "yes" ];
+then
+
+   if [ ! -f ${directory}${sample}/${sample}_R1.fastq.gz] || [ ! -f ${directory}${sample}/${sample}_R2.fastq.gz ]; then
+      echo "Paired-end reads (SAMPLE_R1.fastq.gz or SAMPLE_R2.fastq.gz) do not exist for ${sample}! Skipping analysis . . ."
+      continue
+   fi
+else
+
+   if [ ! -f ${directory}${sample}/${sample}.fastq.gz]; then
+      echo "Reads (SAMPLE.fastq.gz) do not exist for ${sample}! Skipping analysis . . ."
+      continue
+   fi
+fi
+
 
 #Extract adapter and barcode sequences from adapter file
 
