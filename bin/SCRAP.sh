f16ad903ee4f8ce877668fff611149db5cdff41e
sreenieadara <sreeni.eadara@gmail.com>
2023-01-30 15:37:17 -0500

    bug fix

--- bin/SCRAP.sh
+++ bin/SCRAP.sh
@@ -68,7 +68,7 @@ if [ -z "$miRBase_species_abbreviation" ]; then
     exit 1
 fi
 
-if [ ! -f "${reference_directory}/fasta/sncRNA_${miRBase_species_abbreviation}.fasta ]; then
+if [ ! -f "${reference_directory}/fasta/sncRNA_${miRBase_species_abbreviation}.fasta" ]; then
     echo "Error: bad miRBase abbrevation or bad reference installation [-m]"
     exit 1
 fi
